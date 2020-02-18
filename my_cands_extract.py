d22913982d901ddf9d846d9bab57a405cbc51cbf
Stephen Mayhew <stephen@duolingo.com>
2020-02-18 10:29:44 -0500

    Fix issues with ordering of prompts in fairseq-generate. (#5)
    * Fix issues with ordering of prompts in fairseq-generate.
    
    * Address review from Chris

--- my_cands_extract.py
+++ my_cands_extract.py
@@ -1,60 +1,90 @@
 import argparse
-import hashlib
+from collections import defaultdict
 
-from sacremoses import MosesDetokenizer
-
-from utils import FIELDSEP, makeID
+from utils import FIELDSEP, read_trans_prompts
 
 
-def main(infile: str, outfile: str, candlimit: int):
+def main(origfile: str, infile: str, outfile: str, candlimit: int):
     """
     This processes the output of fairseq-generate so that it can be scored with sacrebleu and 
     so that it has the shared task format. 
+
+    fairseq-generate does this annoying thing where the order of the outputs is not guaranteed, but 
+    it retains order information in the metadata. So we just need to keep track of that and use it to
+    order the outputs correctly.
+
     """
 
     sysfile = "sys.out"
     reffile = "ref.out"
 
-    md = MosesDetokenizer(lang="en")
+    with open(origfile) as f:
+        orig_prompts = read_trans_prompts(f.readlines())
 
-    with open(infile) as f, open(outfile, "w") as out, open(sysfile, "w") as sf, open(reffile, "w") as rf:
-        first = True
-        cands = 0
-        for line in f:
-            sline = line.strip().split("\t")
-            if line.startswith("S-"):
-                # it's hard to have fairseq pass prompt ids through the training/evaluation process
-                # so we resort to regenerating ids based on the prompt text.
-                # we have to be careful that the text is *exactly* the same, or the id generation will be wrong.
-                sline_detok = md.detokenize(sline[1].split(" "))
-                textID = makeID(sline_detok)
-                out.write(f"\n{textID}{FIELDSEP}{sline[1]}\n")
-                first = True
-                cands = 0
-            elif line.startswith("T-"):
-                # this is the reference
-                rf.write(sline[1] + "\n")
-            elif line.startswith("H-"):
-                # this is the prediction, there may be many of these.
-                if candlimit == -1 or cands < candlimit:
-                    out.write(sline[2] + "\n")
-                    cands += 1
+    # this variable is used to write out only the top hypothesis for scoring bleu
+    first = True
+    cands = 0
 
-                # only write the first of these.
-                if first:
-                    sf.write(sline[2] + "\n")
-                    first = False
-            else:
-                pass
+    # maps: order from metadata to list of lines to be written to file. 
+    # make sure to sort this by key before writing to file.
+    outd = defaultdict(list)
+    refd = defaultdict(list)
+    sysd = defaultdict(list)
+
+    for line in f:
+        sline = line.strip().split("\t")
+        if line.startswith("S-"):
+            num = int(sline[0].split("-")[-1])
+            outd[num].append(sline[1])
+            first = True
+            cands = 0
+        elif line.startswith("T-"):
+            num = int(sline[0].split("-")[-1])
+            # this is the reference
+            refd[num].append(sline[1] + "\n")
+        elif line.startswith("H-"):
+            num = int(sline[0].split("-")[-1])
+            # this is the prediction, there may be many of these.
+            if candlimit == -1 or cands < candlimit:
+                outd[num].append(sline[2] + "\n")
+                cands += 1
+
+            # only write the first of these.
+            if first:
+                sysd[num].append(sline[2] + "\n")
+                first = False
+        else:
+            pass
+
+    with open(infile) as f, open(outfile, "w") as out, open(sysfile, "w") as sf, open(reffile, "w") as rf:    
+        # orig_prompts has a particular order, and when we sort outd by 
+        # the items, the order should match.
+        for orig_prompt,item in zip(orig_prompts, sorted(outd.items())):
+            num,linelist = item
+            firstline = linelist[0]
+            textID = orig_prompt[0]
+            out.write(f"\n{textID}{FIELDSEP}{firstline}\n")
+
+            for line in linelist[1:]:
+                out.write(line)
+
+        for num,linelist in sorted(sysd.items()):
+            for line in linelist:
+                sf.write(line)
+
+        for num,linelist in sorted(refd.items()):
+            for line in linelist:
+                rf.write(line)
 
     print(f"Wrote to {outfile}, {sysfile}, {reffile}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("This processes the output of fairseq-generate so that it can be scored with sacrebleu and so that it has the shared task format. ")
+    parser.add_argument("--origfile", help="Name of the original shared task file, with original prompt IDs.", required=True)
     parser.add_argument("--infile", help="Name of output file from fairseq-generate, probably called gen.out", required=True)
     parser.add_argument("--outfile", help="Name of desired output file. This will be the shared task format file.", required=True)
     parser.add_argument("--candlimit", help="Max number of candidates to put in file (default is -1, meaning all)", type=int, default=-1)
     args = parser.parse_args()
 
-    main(args.infile, args.outfile, args.candlimit)
+    main(args.origfile, args.infile, args.outfile, args.candlimit)
--- variables.sh
+++ variables.sh
@@ -1,6 +1,6 @@
 # this doesn't change.
 src=en
-tgt=vi
+tgt=hu
 
 lang=${src}-${tgt}
 DATA=data/courses/${lang}
@@ -11,6 +11,9 @@ DUMP=data/dumps/${lang}
 # git clone https://github.com/rsennrich/subword-nmt
 MOSES=$HOME/IdeaProjects/mosesdecoder
 SUBWORDNMT=$HOME/IdeaProjects/subword-nmt
+
+# Location of the shared task data.
+SHARED_TASK_DATA=$HOME/staple2020/staple-2020-train
 
 # used in run_pretrained.sh (don't change NBEST)
 NBEST=10
